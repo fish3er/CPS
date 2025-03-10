9b1959633ec8c2dfab2d3012608cbeef99a34922
fish3er <mateuszrybak631@gmail.com>
2025-03-10 15:58:40 +0100

    początek zad 4

--- lab1zad4.py
+++ lab1zad4.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 #Mateusz = [77,97,116,101,117,115,122]
 fpr = 16000  # Częstotliwość próbkowania 16 kHz
-T = 1  # Czas transmisji bitu w sekundach (100 ms)
+T = 0.1  # Czas transmisji bitu w sekundach (100 ms)
 fc = 500  # Częstotliwość sinusoidy 500 Hz
 #1600 próbek bit
 asci_name= [77,97,116,101,117,115,122]
@@ -12,7 +12,7 @@ bit_name=[]
 for i in asci_name:
     bit_name.append(bin(i))
 print(bit_name)
-time = np.linspace(0, T, int(fpr * T), endpoint=False)
+time = np.linspace(0, T, int(fpr * T * len(bit_name)), endpoint=False)
 sig=[]
 for bit in bit_name:
     if bit[0]=='1':
