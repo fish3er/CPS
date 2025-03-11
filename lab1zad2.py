1db873994aeebcb43d1463590fde804fa1c6874b
fish3er <167292998+fish3er@users.noreply.github.com>
2025-03-11 18:55:45 +0100

    zad 2 zmiana danych

--- lab1zad2.py
+++ lab1zad2.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 fs=200 #czestotliwość próbkowania
 fr=10000 # czętotliwość rekonstrukcji
 T =1/fs # okres próbkowania sygnału początkowego
-t=1
+t=0.1
 ampl=230
 fsig=50 # 50
 #próbkowanie
-t_sample=np.linspace(0,t,fs,endpoint="True")
+t_sample=np.linspace(0,t,int(t*fs),endpoint="True")
 sig_sample=np.sin(2*np.pi*fsig*t_sample)
 sig_sample=sig_sample*ampl
 
-t_rec= np.linspace(0,t,fr) # bo 0.1 z 10kHz
+t_rec= np.linspace(0,t,int(fr*t)) # bo 0.1 z 10kHz
 sig_rec=np.zeros_like(t_rec) # tablica zer
 for n in range(len(t_rec)):
-    sig_rec[n]=np.sum(sig_sample * np.sinc((t_rec[n]-t_sample)/T))
+    sig_rec[n]=np.sum(sig_sample * np.sinc((t_rec[n]-t_sample)/T)) # pi jest we wzorze
 
 sig_analog=ampl*np.sin(2*np.pi*fsig*t_rec)
 error = sig_analog - sig_rec
